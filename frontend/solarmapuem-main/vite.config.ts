15a9dd3c79cbf72e0467a7a0f248f033ba39a4f0
guillermoanguloblanco <guillermo.anguloblanco@gmail.com>
2026-05-12 21:50:54 +0200

    poner el mapa en la web

--- frontend/solarmapuem-main/vite.config.ts
+++ frontend/solarmapuem-main/vite.config.ts
@@ -17,4 +17,4 @@ export default defineConfig({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },  
-});
+}); 
